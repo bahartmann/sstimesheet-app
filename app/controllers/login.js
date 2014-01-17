0329b02824d6659abe092021abba9f64bc642c21
bahartmann <barbarahartma@gmail.com>
2014-01-17 10:50:37 -0200

    Login view

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,3 +1,22 @@
 exports.show = function() {
 	$.loginWindow.open();
 };
+
+$.confirmButton.addEventListener("click", function(){
+	userLogin();
+});
+
+function userLogin() {
+	if ($.usernameField.value == "teste" && $.passwordField.value == "teste"){
+		alert("Aeeeee");
+	} else {
+		alert("Usuário ou senha incorretos.");
+	}
+}
+ 
+$.loginWindow.addEventListener('click', function(evt) {
+	Ti.API.info("evt.source: " + evt.source);
+	if (evt.source == $.loginWindow || evt.source == $.loginScroll) {
+		$.loginWindow.close();
+	}
+});
