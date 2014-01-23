f21b097755e3ce1ec100623db222f3870c7f847a
bahartmann <barbarahartma@gmail.com>
2014-01-23 12:57:15 -0200

    Login with connection

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,18 +3,40 @@ exports.show = function() {
 };
 
 $.confirmButton.addEventListener("click", function(){
-	userLogin();
+	connection();
 });
 
-function userLogin() {
-	if ($.emailField.value == "teste" && $.passwordField.value == "teste"){
-		var monthController = Alloy.createController('month');
-		monthController.show();
-	} else {
-		alert("Invalid email or password.");
-	}
+function connection() {
+	var url = "http://sstimesheet.herokuapp.com/month.json";
+	var client = Ti.Network.createHTTPClient({
+		onload : function(e) {
+			Ti.API.info("Received text: " + this.responseText);
+			Ti.API.info("Received status: " + this.status);
+			var toast = Titanium.UI.createNotification({
+				duration: 700,
+				message: "Connected"
+			}).show();
+			var monthController = Alloy.createController('month');
+			monthController.show();
+		},
+		onerror : function(e) {
+			Ti.API.info("Received text: " + this.responseText);
+			Ti.API.info("Error: " + this.status);
+			var toast = Titanium.UI.createNotification({
+				duration: 700,
+				message: "Connection error"
+			}).show();
+		},
+		timeout : 5000
+	});
+	var params = {  
+    	user_email: $.emailField.value,
+		user_password: $.passwordField.value
+	}; 
+	client.open("GET", url);
+	client.send(params);
 }
- 
+
 $.loginWindow.addEventListener('click', function(evt) {
 	if (evt.source == $.loginWindow || evt.source == $.loginScroll) {
 		$.loginWindow.close();
